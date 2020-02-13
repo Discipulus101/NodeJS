741f98c9595881fa3e1d25690fcade3558c161c0
Discipulus101 <Discipulus101@users.noreply.github.com>
2020-02-13 15:07:40 +0300

    lesson-6

---
+++ auth.js
@@ -0,0 +1,42 @@
+const passport = require('passport');
+const Strategy = require('passport-local').Strategy;
+
+const User = require('./models/user');
+
+passport.use(new Strategy(async (username, password, done) => {
+  const user = await User.findOne({username});
+
+  if (!user) {
+    console.log('NO NAME!')
+    return done(null, false);
+  }
+
+  if (!user.comparePassword(password)) {
+    console.log('NO PASSSWORD!')
+    return done(null, false);
+  }
+
+  const plainUser = JSON.parse(JSON.stringify(user));
+  delete plainUser.password;
+  done(null, plainUser); // req.user
+}));
+
+passport.serializeUser((user, done) => {
+  return done(null, user._id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  const user = await User.findById(id);
+  const plainUser = JSON.parse(JSON.stringify(user));
+  delete plainUser.password;
+  done(null, plainUser); // req.user
+});
+
+module.exports = {
+  initialize: passport.initialize(),
+  session: passport.session(),
+  authenticate: passport.authenticate('local', {
+    successRedirect: './tasks',
+    failureRedirect: '/auth?error=1',
+  }),
+};
